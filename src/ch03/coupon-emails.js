a1d928cf2845d1bee0e92ff90cb2c3029e441194
Juraj Martinka <jumarko@gmail.com>
2022-02-03 06:54:27 +0100

    Chapter 3: finish code for coupon emails with sendIssue function.

--- src/ch03/coupon-emails.js
+++ src/ch03/coupon-emails.js
@@ -39,4 +39,57 @@ selectCouponsByRank([coupon, {code: "goodone", rank: "good"}, {code: "BESTOFALL"
 [ { code: 'BESTOFALL', rank: 'best' } ]
 
 
-// TODO: email (p. 49)
+// email (p. 49) - it's just data:
+const message = {
+  from: "nesletter@coupondog.co",
+  to: "sam@pmail.com",
+  subject: "Your weekly coupons inside",
+  body: "Here are your coupons"
+};
+
+// this is a _calculation_
+function emailForSubscriber(subscriber, goods, bests) {
+  const rank = subscriberCouponRank(subscriber);
+  if (rank == "best") {
+    return {
+      from: "nesletter@coupondog.co",
+      to: subscriber.email,
+      subject: "Your best weekly coupons inside",
+      body: "Here are the best coupons: " + bests.join(", ")
+    };
+  } else {
+    return {
+      from: "nesletter@coupondog.co",
+      to: subscriber.email,
+      subject: "Your good weekly coupons inside",
+      body: "Here are the good coupons: " + goods.join(", ")
+    };
+  }
+}
+
+// here I'm intentionally skipping `emailsForSubscribers`
+// because I think it brings little value - better to use higher-level functions ala `map`
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map
+[subscriber].map(emailForSubscriber);
+
+
+// the final Calculation that ties it all together!
+// ... and some stubs for other actions like fetching from the db;
+function fetchCouponsFromDb() {
+  return [coupon];
+}
+function fetchSubscribersFromDb() {
+  return [subscriber];
+}
+const emailSystem = {
+  send: function(email) {}
+};
+function sendIssue() {
+  const coupons = fetchCouponsFromDb();
+  const goodCoupons = selectCouponsByRank(coupons, "good");
+  const bestCoupons = selectCouponsByRank(coupons, "best");
+  const subscribers = fetchSubscribersFromDb();
+
+  const emails = subscribers.map(emailForSubscriber);
+  emails.forEach(emailSystem.send);
+}
